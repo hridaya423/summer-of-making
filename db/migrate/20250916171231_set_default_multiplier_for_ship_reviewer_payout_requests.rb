c2a549b0f1a715b1c980105c31882c1ec751266e
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2025-09-16 13:19:16 -0400

    Robo Cop Lint fix

--- db/migrate/20250916171231_set_default_multiplier_for_ship_reviewer_payout_requests.rb
+++ db/migrate/20250916171231_set_default_multiplier_for_ship_reviewer_payout_requests.rb
@@ -1,7 +1,7 @@
 class SetDefaultMultiplierForShipReviewerPayoutRequests < ActiveRecord::Migration[8.0]
   def change
     change_column_default :ship_reviewer_payout_requests, :multiplier, 1.0
-    
+
     # Backfill existing records with the default value
     execute "UPDATE ship_reviewer_payout_requests SET multiplier = 1.0 WHERE multiplier IS NULL"
   end

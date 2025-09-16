cac2f57b80e4bb65ae28a12fe10d51ca2ef08c05
Dhamari Trice-Hanson <39872667+dhamariT@users.noreply.github.com>
2025-09-16 13:20:43 -0400

    Wrap `execute` statement in `safety_assured` block during default multiplier backfill.

--- db/migrate/20250916171231_set_default_multiplier_for_ship_reviewer_payout_requests.rb
+++ db/migrate/20250916171231_set_default_multiplier_for_ship_reviewer_payout_requests.rb
@@ -3,6 +3,8 @@ class SetDefaultMultiplierForShipReviewerPayoutRequests < ActiveRecord::Migratio
     change_column_default :ship_reviewer_payout_requests, :multiplier, 1.0
 
     # Backfill existing records with the default value
-    execute "UPDATE ship_reviewer_payout_requests SET multiplier = 1.0 WHERE multiplier IS NULL"
+    safety_assured do
+      execute "UPDATE ship_reviewer_payout_requests SET multiplier = 1.0 WHERE multiplier IS NULL"
+    end
   end
 end

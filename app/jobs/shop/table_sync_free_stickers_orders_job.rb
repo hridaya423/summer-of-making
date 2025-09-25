606fdc0b5238c003c0470c67767e4120b201f5b7
Sam Liu <sam@samliu.dev>
2025-09-24 23:56:07 -0400

    Move free stickers sync to another queue

--- app/jobs/shop/table_sync_free_stickers_orders_job.rb
+++ app/jobs/shop/table_sync_free_stickers_orders_job.rb
@@ -1,5 +1,5 @@
 class Shop::TableSyncFreeStickersOrdersJob < ApplicationJob
-  queue_as :literally_whenever
+  queue_as :free_stickers_sync
 
   def perform(*args)
     ShopOrder.mirror_free_stickers_orders_to_airtable! "s1GhHahY"
